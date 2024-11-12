ab1fef934e323a9eb415860d0b7c9acf7ebc7ef6
Matias A. <mt.alexander.go@gmail.com>
2024-11-12 13:02:07 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+// 3 things
+// Persistent cookie for any user that accesess the website
+//
+// Database to store the tasks
+//
+// UI to create, change and delete tasks
+
+console.log("Hello World");
