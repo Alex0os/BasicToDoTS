1c4aea1a63215dc015144c0add59d1fde0b454d3
Matias A. <mt.alexander.go@gmail.com>
2024-11-15 22:51:01 -0300

    feat: "public" directory for dedicated frontend static files
    - Also, in the "index.ts" file was implemented the logic to access the
      index.html file to send to the client

--- index.ts
+++ index.ts
@@ -5,15 +5,12 @@
 //
 // UI to create, change and delete tasks
 
-// First, the codestyle, then the presentation page and how to store the HTML
-// and CSS inside this project and then we do the thing with cookies
 import * as http from "node:http";
+import * as fs from "fs";
+import path, { join } from "node:path";
 
-const validUris = ["/"];
-const COOKIE_TIMEOUT = 30 // 30 seconds
+const COOKIE_TIMEOUT = 1 // 30 seconds
 
-// Due to the method "writeHead()" will write another header fields, the key
-// signature in the interface its necessary to make it done
 
 interface ResponseHeader {
 	[key: string]: string | undefined;
@@ -24,40 +21,35 @@ interface ResponseHeader {
 
 const server = http.createServer((req, res) => {
 	let sessionId = Math.floor(new Date().getTime() / 1000).toString(); // Date in seconds
-	let cookie : string | undefined = req.headers.cookie;
-	let uri : string | undefined = req.url;
-
+	let reqCookie = req.headers.cookie;
+	let reqUri = req.url;
 
 	const response_header: ResponseHeader = {
-		"content-type": "text/plain",
+		"content-type": "text/html",
 		"Access-Control-Allow-Origin": "*",
 	}
 
-	if (uri && validUris.includes(uri) && cookie)
-		console.log(`${uri} -> ${cookie}`);
+	const parentFolder = path.resolve(__dirname, "..");
+	let body = "Hello world without HTML";
 
-	else if (uri && !validUris.includes(uri))
-		console.log(`${uri} is not valid for cookies`);
-
-	else if (uri && validUris.includes(uri) && !cookie) {
-		console.log("This client hasn't set any cookie for this url: " + uri);
+	if (reqUri === "/" && !reqCookie) {
+		body = fs.readFileSync(join(parentFolder, "public", "index.html"), "utf8")
 
 		response_header["set-cookie"] =
 		"sessionId=" + sessionId + "; Path=/; HttpOnly; Secure; Max-Age=" 
 		+ COOKIE_TIMEOUT.toString() + ";" + "SameSite=Strict";
 
-		console.log("Cookie " + response_header["set-cookie"] 
-					+ "\nwill be send to the client");
+		console.log("Cookie: " + response_header["set-cookie"] +
+					"\nwill be send to the client");
 	}
 
 	res
 	.writeHead(200, response_header)
-	.end("Hello World\n"); // Specifies no more data will be send in this response
+	.end(body); // Specifies no more data will be send in this response
 })
 
 
 server.listen(8080, () => {
 	console.log("HTTP server init successfully");
 });
-
 
