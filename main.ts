3311127544213f42228025dec5326c509da06558
Matias A. <mt.alexander.go@gmail.com>
2024-11-19 00:29:17 -0300

    feat: Now the users table will store the cookie and the cookie timestamp
    - Also, a new function "cookieTimeStamp()" was created and will be
      implemeneted to calculate the cookie life span, to then be stored in
      the table, with the purpose to mark the row as death if the current
      date time is over the cookie time stamp

--- main.ts
+++ main.ts
@@ -8,8 +8,9 @@
 import * as http from "node:http";
 import * as fs from "fs";
 import path, { join } from "node:path";
+import { connect } from "ts-postgres";
 
-const COOKIE_TIMEOUT = 1 // 30 seconds
+const COOKIE_TIMEOUT = 1 // 31 seconds
 
 
 interface ResponseHeader {
@@ -20,23 +21,23 @@ interface ResponseHeader {
 }
 
 const server = http.createServer((req, res) => {
-	let sessionId = Math.floor(new Date().getTime() / 1000).toString(); // Date in seconds
 	let reqCookie = req.headers.cookie;
 	let reqUri = req.url;
+	let body = "Hello world without HTML";
 
 	const response_header: ResponseHeader = {
 		"content-type": "text/html",
 		"Access-Control-Allow-Origin": "*",
 	}
 
-	const parentFolder = path.resolve(__dirname, "..");
-	let body = "Hello world without HTML";
 
 	if (reqUri === "/" && !reqCookie) {
+		const cookieId = Math.floor(new Date().getTime() / 1000).toString(); // Date in seconds
+		const parentFolder = path.resolve(__dirname, "..");
 		body = fs.readFileSync(join(parentFolder, "public", "index.html"), "utf8")
 
 		response_header["set-cookie"] =
-		"sessionId=" + sessionId + "; Path=/; HttpOnly; Secure; Max-Age=" 
+		"sessionId=" + cookieId + "; Path=/; HttpOnly; Secure; Max-Age=" 
 		+ COOKIE_TIMEOUT.toString() + ";" + "SameSite=Strict";
 
 		console.log("Cookie: " + response_header["set-cookie"] +
@@ -49,7 +50,50 @@ const server = http.createServer((req, res) => {
 })
 
 
-server.listen(8080, () => {
-	console.log("HTTP server init successfully");
-});
+// TODO: Configure the psqlConnection to create the table and use it to store
+// users that connects to the main page of the server.
+//
+// TODO: Due to the fact that cookies expire after a certain amount of time,
+// then a script should be write to delete users where their cookie has
+// expired, thus creating a field that contains the expire date of the cookie
+// is a must in this case.
+// Function to add time to the current date
+
+
+function cookieTimeStamp(): string {
+	let date = new Date().getTime();
+	date += COOKIE_TIMEOUT * 1000;
+
+	let newDate = new Date(date);
+	return newDate.toISOString().replace("T", " ").replace(/\..*$/, "") + ":+00"; 
+}
+
+async function createDB() {
+	const psqlConnection = await connect({
+		user: "Matixannder",
+		host: "localhost",
+		port: 5432,
+		database: "TODOAppDB",
+		keepAlive: true
+	});
+
+	// This is the command that I'll use when I have to compare the timestamps
+	// select * from users 
+	// where cookie_expiration_date < NOW() at time zone 'utc';
+	const createUserTable =
+		`CREATE TABLE IF NOT EXISTS users (
+			id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+			cookie_id VARCHAR(10),
+			cookie_expiration_date_utc TIMESTAMP
+	);`; 
+
+	await psqlConnection.query(createUserTable);
+	// Create table only if table doesn't exists already
+
+	server.listen(8080, () => {
+		console.log("HTTP server init successfully");
+	});
+}
+
+createDB();
 
